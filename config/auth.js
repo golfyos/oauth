e41005c49697a540a81d308c6a5474b5d31ae53e
golf <golf_cat1@hotmail.com>
2016-10-24 16:32:41 +0700

    edit authen callback

--- config/auth.js
+++ config/auth.js
@@ -6,19 +6,19 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1220751207996602', // your App ID
         'clientSecret'  : 'f700d414aa13ddded24edd473cd12d68', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://ec2-54-255-167-119.ap-southeast-1.compute.amazonaws.com:8080/auth/facebook/callback'
     },
 
     'twitterAuth' : {
         'consumerKey'       : 'FVe4QJYBmYTNhlGMkEwYeWwqd',
         'consumerSecret'    : 'tFI7glsQknGuC1iWbtUm4GqvMNs6rkRrz84PfYk0dSB5yiN08o',
-        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+        'callbackURL'       : 'http://ec2-54-255-167-119.ap-southeast-1.compute.amazonaws.com:8080/auth/twitter/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '491509167174-c5dbi2r8cro4u11d22ln97i9l61bvgj7.apps.googleusercontent.com',
         'clientSecret'  : 'q8hgsGRa34pej_yYhGaT7qdS',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'callbackURL'   : 'http://ec2-54-255-167-119.ap-southeast-1.compute.amazonaws.com:8080/auth/google/callback'
     }
 
 };
